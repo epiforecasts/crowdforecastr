62eff4d2ec0db35691a94c5acfbf5c0295b9a991
nikosbosse <nikosbosse@gmail.com>
2021-01-23 08:52:00 +0100

    delete unnecessary files

--- inst/sandbox_test.R
+++ inst/sandbox_test.R
@@ -20,32 +20,15 @@ obs_filt <- observations
 data.table::fwrite(x = obs_filt, file = "external_ressources/observations.csv")
 obs_filt <- data.table::fread("external_ressources/observations.csv")
 
-# # copy data into human forecast app
-# file.copy(from = here::here("../covid-german-forecasts/human-forecasts/processed-forecast-data"), 
-#           to = here::here("external_ressources/"), recursive = TRUE)
-# 
-# past_forecasts <- crowdforecastr:::load_past_forecasts("external_ressources/processed-forecast-data/")
-# data.table::fwrite(x = past_forecasts, file = "external_ressources/past_forecasts.csv")
-
-
-
 obs_filt <- data.table::fread("external_ressources/observations.csv")  %>%
   dplyr::mutate(target_end_date = as.Date(target_end_date)) %>%
   dplyr::arrange(location, target_type, target_end_date) 
 
-past_forecasts <- data.table::fread("external_ressources/past_forecasts.csv") %>%
-  dplyr::mutate(target_end_date = as.Date(target_end_date)) %>%
-  dplyr::mutate(target_type = ifelse(target_type == "case", "cases", "deaths"))
-
-past_forecasts <- dplyr::full_join(past_forecasts, obs_filt %>%
-                                     dplyr::rename(true_value = value)) %>%
-  dplyr::filter(!is.na(prediction))
 
 run_app(data = obs_filt, 
         google_account_mail = "epiforecasts@gmail.com", 
         forecast_sheet_id = "1nOy3BfHoIKCHD4dfOtJaz4QMxbuhmEvsWzsrSMx_grI",#"1xdJDgZdlN7mYHJ0D0QbTcpiV9h1Dmga4jVoAg5DhaKI",
         user_data_sheet_id = "1GJ5BNcN1UfAlZSkYwgr1-AxgsVA2wtwQ9bRwZ64ZXRQ", 
-        past_forecasts = past_forecasts,
         submission_date = "2020-01-11")
 
 
